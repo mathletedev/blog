8fc451d9e8b054df84c4958963c4b4b1d2763662
Neal Wang <nealwang.sh@protonmail.com>
2023-04-01 21:04:59 -0700

    added work section, re-css'ed everything

--- src/lib/scroll.ts
+++ src/lib/scroll.ts
@@ -1,20 +1,10 @@
 const content = document.getElementById("scroll")!;
 const tip = document.getElementById("tip")!;
 
-const TEXT = `
-Neal Wang is a high schooler with over 6 years of coding experience.
-He made this portfolio site using Astro, TypeScript, and TailwindCSS.
-As someone who learns a lot, he loves to teach and inspire others.
-Neal runs an educational programming YouTube channel, MathleteDev, with over 300 subscribers.
-He is part of his school's IT team and loves to help teachers with technology issues.
-In his free time, Neal enjoys athletics such as playing tennis and swimming.
-He is part of Pullman High School's Math Team, Science Olympiad, Computer Science Club, and 4061 Robotics team.
-He is a great violin player and artist.
-Not quite so great at chess, but he likes to play it.
-Furthermore, Neal is an avid Linux (NixOS) user and advocate for open source software.
-Most of his projects are uploaded to GitHub under the GPL open source license.
-Neal hopes to use these skills to make the world a better place!
-`;
+let TEXT = "";
+export const setup = (text: string) => {
+	TEXT = text;
+};
 
 let index = 0;
 
