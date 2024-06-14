9ef2748af4f80c49513d43c77d605fc9056ed330
Neal Wang <nealwang.sh@protonmail.com>
2024-06-13 21:51:49 -0700

    rainbow animations!

--- src/components/TypistLoop.tsx
+++ src/components/TypistLoop.tsx
@@ -2,30 +2,30 @@ import React, { FC } from "react";
 import Typist from "react-typist-component";
 
 export interface Props {
-    texts: string[];
+	texts: string[];
 }
 
 const shuffle = (array: string[]) =>
-    array
-        .map(value => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value);
+	array
+		.map(value => ({ value, sort: Math.random() }))
+		.sort((a, b) => a.sort - b.sort)
+		.map(({ value }) => value);
 
 const TypistLoop: FC<Props> = ({ texts }) => (
-    <div className="h-24">
-        <Typist typingDelay={100} loop>
-            {shuffle(texts).map(text => (
-                <div key={text}>
-                    <Typist.Delay ms={1000} />
-                    <p className="text-center text-2xl md:text-4xl text-sapphire italic opacity-80 select-none neon">
-                        {text}
-                    </p>
-                    <Typist.Delay ms={1500} />
-                    <Typist.Backspace count={text.length} />
-                </div>
-            ))}
-        </Typist>
-    </div>
+	<div className="h-24">
+		<Typist typingDelay={100} loop>
+			{shuffle(texts).map(text => (
+				<div key={text}>
+					<Typist.Delay ms={1000} />
+					<p className="text-center text-2xl md:text-4xl italic opacity-80 select-none rainbow-text neon">
+						{text}
+					</p>
+					<Typist.Delay ms={1500} />
+					<Typist.Backspace count={text.length} />
+				</div>
+			))}
+		</Typist>
+	</div>
 );
 
 export default TypistLoop;
