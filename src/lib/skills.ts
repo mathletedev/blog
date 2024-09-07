974678466218d25aae379ae46e447c0e48ba77f3
Neal Wang <nealwang.sh@protonmail.com>
2024-09-07 15:45:20 -0700

    feat: update skills

--- src/lib/skills.ts
+++ src/lib/skills.ts
@@ -1,33 +1,34 @@
 import { CHART_COLOURS, createChart } from "./chart";
 
 const skills = document.getElementById("skills") as HTMLCanvasElement;
-const nav = document.getElementById("fade")!.children as HTMLCollectionOf;
+const nav = document.getElementById("fade")!
+    .children as HTMLCollectionOf<HTMLElement>;
 
 skills.width = skills.height = Math.min(500, window.innerWidth - 32);
 
-const CHARTS: Record[] = [
+const CHARTS: Record<string, number>[] = [
     {
         TypeScript: 10,
         Rust: 9,
         "C/C++": 8,
-        Python: 7,
-        Java: 8,
-        Go: 5
-    },
-    {
-        "GNU/Linux": 10,
-        Windows: 8,
-        Nix: 9,
-        Bash: 8,
-        Vim: 10,
-        Git: 8
+        Go: 7,
+        Gleam: 6,
+        Python: 5
     },
     {
         Astro: 10,
         Svelte: 9,
         Tailwind: 10,
+        GraphQL: 9,
         tRPC: 8,
         PostgreSQL: 9
+    },
+    {
+        "GNU/Linux": 10,
+        Nix: 9,
+        Neovim: 10,
+        Git: 9,
+        Jira: 7
     }
 ];
 
